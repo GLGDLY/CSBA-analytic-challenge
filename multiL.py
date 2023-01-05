505e57740d88bdc91eef5085ed07f3b96760df51
VYPang <121414357+VYPang@users.noreply.github.com>
2023-01-05 14:31:17 +0800

    Update MultiL.py
    show HSI graph

--- multiL.py
+++ multiL.py
@@ -2,12 +2,22 @@ from json import loads
 import pandas as pd
 from sklearn import linear_model
 import statsmodels.api as sm
+import plotly.graph_objects as go
 
-with open("dataset1_stats.txt", "r", encoding="utf-8") as f:
-  datas = f.read().split("\n")
-  headline_monthly_ESG = loads(datas[9])
+with open("./datas/dataset1_stats.txt", "r", encoding="utf-8") as f:
+    datas = f.read().split("\n")
+    headline_monthly_9ESG = loads(datas[12])
+stat_df = pd.DataFrame(headline_monthly_9ESG).T
+stat_df['max'] = stat_df.idxmax(axis="columns")
 
+HSI_df = pd.read_csv("./datas/^HSI_weekly.csv")
 
+fig = go.Figure(data=[go.Candlestick(x=HSI_df['Date'],
+                open=HSI_df['Open'],
+                high=HSI_df['High'],
+                low=HSI_df['Low'],
+                close=HSI_df['Close'])])
+fig.show()
 
 """
 x = df[['interest_rate','unemployment_rate']]
