bcd84410a3910caf02d5fd038f651927c5705f0e
Ronic725 <91591480+Ronic725@users.noreply.github.com>
2023-01-10 14:58:10 +0800

    plot selected ESG with HSI ; orgranize files

--- plotss.py
+++ plotss.py
@@ -12,9 +12,14 @@ df.rename(columns = {'pubdate': 'date'}, inplace = True)
 #removing day from the date column
 df['date'] = pd.to_datetime(df['date'])
 df['date'] = df['date'].apply(lambda x: x.strftime('%Y-%m')) #only year and month is left
+
+#new changes: choose statistically signficant variable only 
+new_df = df.drop(['Corporate Governance', 'Natural Capital', 'Human Capital','Business Ethics & Values', 'Non-ESG'], axis=1)
+
+#turn the classes into one columns
 df = df.melt('date')
 df.rename(columns = {'variable': 'class', 'value': 'sentiment score'}, inplace = True)
-print(df)
+#print(df)
 
 base = alt.Chart(df).mark_line().encode(
   x='date:T',
@@ -24,3 +29,52 @@ base = alt.Chart(df).mark_line().encode(
 
 base.show()
 
+
+#creat chart for HSI 
+HSI_df = pd.read_csv("./datas/HSI_1.csv")
+#HSI_df.drop(['High','Low', 'Close', 'Adj Close', 'Volume'], axis=1)
+#HSI_df = HSI_df.set_index(new_df.index)
+#new_df['Open price'] = HSI_df.iloc[:,1].tolist()
+open_close_color = alt.condition("datum.Open <= datum.Close",
+                                 alt.value("#06982d"),
+                                 alt.value("#ae1325"))
+
+base = alt.Chart(HSI_df).encode(
+    alt.X('Date:T',
+          axis=alt.Axis(
+              format='%m/%d',
+              labelAngle=-45,
+              title='Date'
+          )
+    ),
+    color=open_close_color
+)
+
+rule = base.mark_rule().encode(
+    alt.Y(
+        'Low:Q',
+        title='Price',
+        scale=alt.Scale(zero=False),
+    ),
+    alt.Y2('High:Q')
+)
+
+bar = base.mark_bar().encode(
+    alt.Y('Open:Q'),
+    alt.Y2('Close:Q')
+)
+
+base1 = (rule + bar)
+
+#plot line diagram, with stat sign var only 
+new_df = new_df.melt('date')
+new_df.rename(columns = {'variable': 'class', 'value': 'sentiment score'}, inplace = True)
+#print(new_df)
+base2 = alt.Chart(new_df).mark_line().encode(
+  x='date:T',
+  y='sentiment score:Q',
+  color='class'
+).interactive(bind_y=False)
+
+#combine base 1 and 2 and show
+alt.layer(base1, base2).resolve_scale(y='independent').show()
--- test.py
+++ test.py
@@ -1,30 +1,36 @@
-# python3.10 -u test.py
-"""
-from vega_datasets import data
-stocks = data.stocks()
-print(stocks)
-
-
 import altair as alt
-alt.Chart(stocks).mark_line().encode(
-  x='date:T',
-  y='price',
-  color='symbol'
-).interactive(bind_y=False)
-"""
-import altair as alt
-from altair.expr import datum
-
 from vega_datasets import data
-stocks = data.stocks.url
+#python3.10 -u test.py
+source = data.ohlc()
+print(source)
 
-base = alt.Chart(stocks).encode(
-    x='date:T',
-    y='price:Q',
-    color='symbol:N'
-).transform_filter(
-    datum.symbol == 'GOOG'
+open_close_color = alt.condition("datum.open <= datum.close",
+                                 alt.value("#06982d"),
+                                 alt.value("#ae1325"))
+
+base = alt.Chart(source).encode(
+    alt.X('date:T',
+          axis=alt.Axis(
+              format='%m/%d',
+              labelAngle=-45,
+              title='Date in 2009'
+          )
+    ),
+    color=open_close_color
 )
 
-output = base.mark_line() + base.mark_point()
-output.show()
+rule = base.mark_rule().encode(
+    alt.Y(
+        'low:Q',
+        title='Price',
+        scale=alt.Scale(zero=False),
+    ),
+    alt.Y2('high:Q')
+)
+
+bar = base.mark_bar().encode(
+    alt.Y('open:Q'),
+    alt.Y2('close:Q')
+)
+
+(rule + bar).show()
